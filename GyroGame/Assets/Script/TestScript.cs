0e96c166505ea8ea99b45c003ff3cbee2b85b4a0
KoenigSimon <koenig.simon@t-online.de>
2019-10-03 17:56:10 +0200

    Sync Test

---
+++ GyroGame/Assets/Script/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate obj
+        transform.Rotate(new Vector3(0.5f, 0.5f, 0.5f) * Time.deltaTime * 50);
+    }
+}
