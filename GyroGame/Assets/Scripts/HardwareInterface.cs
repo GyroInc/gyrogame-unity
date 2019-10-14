e6493866d4703034ce517be55e1aeca1a263abb0
KoenigSimon <koenig.simon@t-online.de>
2019-10-14 08:36:04 +0200

    Implemented autoconnect

--- GyroGame/Assets/Scripts/HardwareInterface.cs
+++ GyroGame/Assets/Scripts/HardwareInterface.cs
@@ -2,54 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using System.Threading;
 
+//responsible for handling and maintaining connection to the Cube and interface functions
 public class HardwareInterface : MonoBehaviour
 {
+    //there must only be one
     public static HardwareInterface active;
-    SerialPort port = new SerialPort();
 
-    public Vector3 cubeRot;
+    public bool connectionEstablished;
+
+    SerialPort port;
+    Thread connectionHandler;
+    bool abortConnect = false;
+
+    static int baudRate = 57600;
 
     private void Start()
     {
         active = this;
-        string[] names = SerialPort.GetPortNames();
 
-        for (int i = 0; i < names.Length; i++)
-        {
-            print(names[i]);
-        }
-
-        port.PortName = "COM4";
-        port.BaudRate = 115200;
-        port.ReadTimeout = 500;
-        port.WriteTimeout = 500;
-        port.Close();
-        port.Open();
+        connectionHandler = new Thread(OpenConnection);
+        connectionHandler.Start();
     }
 
     private void Update()
     {
-        if(port.BytesToRead > 0)
-        {
-            string msg = "";
-            while(port.BytesToRead > 0)
-            {
-                msg += port.ReadLine();
-            }
-            if (msg.Length < 5) return;
-            string[] parts = msg.Split('-');
-            float x, y, z;
-            x = float.Parse(parts[0]);
-            y = float.Parse(parts[1]);
-            z = float.Parse(parts[2]);
+        if (port == null) return;
+        if (!port.IsOpen) return;
 
-            cubeRot = new Vector3(x, y, z);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            port.WriteLine("fbt1000");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            port.WriteLine("far255g0b0t1000");
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            port.WriteLine("far0g255b0t1000");
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            port.WriteLine("far0g0b255t1000");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            port.WriteLine("ar200g60b0");
+        }
+    }
+    
+    void OpenConnection()
+    {
+        while (!abortConnect)
+        {
+        //connect to a cube by handshaking with the second software
+        string[] names = SerialPort.GetPortNames();
+
+            //cycle through all the com ports
+            for (int i = 0; i < names.Length; i++)
+            {
+                port = new SerialPort(names[i], baudRate);
+                port.ReadTimeout = 100;
+                port.WriteTimeout = 100;
+                try
+                {
+                    port.Open();
+                    if (port.IsOpen)
+                    {
+                        port.WriteLine("cc");
+                        if (port.ReadLine().Contains("y"))
+                        {
+                            print("success at " + names[i]);
+                            connectionEstablished = true;
+                            port.WriteLine("ar0g0b0");
+                        }
+                        return;
+                    }
+                } catch { print(names[i] + " failed"); }
+            }
         }
     }
 
     private void OnApplicationQuit()
     {
-        port.Close();
+        if(port != null)
+            port.Close();
+        abortConnect = true;
+        connectionHandler.Abort();
     }
 }
