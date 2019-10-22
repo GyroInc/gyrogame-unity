2b0b2f84bf7713a43abfb22e2f98e3a9982d6bf5
KoenigSimon <koenig.simon@t-online.de>
2019-10-22 14:05:46 +0200

    Added Menu

---
+++ GyroGame/Assets/Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public bool random;
+    public Vector3 direction;
+    public float speed;
+
+    private void Start()
+    {
+        if (random)
+            direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+
+    void Update()
+    {        
+        transform.Rotate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    public void Randomize()
+    {
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+}
---
+++ GyroGame/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour
+{
+
+    public float centerOffset;
+    [Space]
+    public Transform selectionCube1;
+    public Transform selectionCube2;
+
+    [Space]
+    public Transform start;
+    public Transform connect;
+    public Transform instructions;
+    public Transform readme;
+    public Transform quit;
+
+    void Update()
+    {
+        
+    }
+
+    public void RemoveSelection()
+    {
+        selectionCube1.position = start.position + (Vector3.up * 10);
+        selectionCube2.position = start.position + (Vector3.up * 10);
+    }
+
+    public void SelectStart()
+    {
+        selectionCube1.position = start.position + (Vector3.left * centerOffset);
+        selectionCube2.position = start.position + (Vector3.right * centerOffset);
+        RandomizeCube();
+    }
+    public void SelectConnect()
+    {
+        selectionCube1.position = connect.position + (Vector3.left * centerOffset);
+        selectionCube2.position = connect.position + (Vector3.right * centerOffset);
+        RandomizeCube();
+    }
+    public void SelectInstructions()
+    {
+        selectionCube1.position = instructions.position + (Vector3.left * centerOffset);
+        selectionCube2.position = instructions.position + (Vector3.right * centerOffset);
+        RandomizeCube();
+    }
+    public void SelectReadme()
+    {
+        selectionCube1.position = readme.position + (Vector3.left * centerOffset);
+        selectionCube2.position = readme.position + (Vector3.right * centerOffset);
+        RandomizeCube();
+    }
+    public void SelectQuit()
+    {
+        selectionCube1.position = quit.position + (Vector3.left * centerOffset);
+        selectionCube2.position = quit.position + (Vector3.right * centerOffset);
+        RandomizeCube();
+    }
+
+    void RandomizeCube()
+    {
+        selectionCube1.GetComponent<Rotate>().Randomize();
+        selectionCube2.GetComponent<Rotate>().Randomize();
+    }
+}
