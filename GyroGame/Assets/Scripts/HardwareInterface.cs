bd932b93b633f10e43fa7c3848163dc90c634ebf
Manuel Piroch <manuel@pepit.net>
2019-11-26 17:48:17 +0100

    Finished initial implementation of HardwareInterface #GG-45 work 2h

--- GyroGame/Assets/Scripts/HardwareInterface.cs
+++ GyroGame/Assets/Scripts/HardwareInterface.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
-using System.IO.Ports;
-using System.Threading;
-using System;
 using System.Globalization;
+using System.IO.Ports;
+using System.Security.Permissions;
+using System.Threading;
+using UnityEngine;
 
 
 //responsible for handling and maintaining connection to the Cube and interface functions
@@ -13,22 +13,30 @@ public class HardwareInterface : MonoBehaviour
     //there must only be one
     public static HardwareInterface active;
 
+    [Header("Preferences")]
     public int baudRate = 38400;
+    public int defaultBrightness;
+    public float connectionTimeout;
+
+    [Header("Debug Settings")]
     public bool debugQuaternion;
     public bool fixedCOMPort;
     public string debugCOMPort;
+
+    [Header("Status Information")]
+    public bool connected;
+    public Quaternion cubeRotation;
+
+    private int voltage, brightness;
+    private bool connectionAttempt;
+    private float cubeTimeoutTimer;
 
     private Thread connectionHandlerThread;
     private Thread communicationHandlerThread;
 
     private SerialPort port;
-    private bool abortConnect = false;
     Queue<string> inMessages = new Queue<string>();
     Queue<string> outMessages = new Queue<string>();
-
-    public Quaternion cubeRotation;
-    public int voltage;
-    public bool connected;
 
     private void Start()
     {
@@ -39,6 +47,7 @@ public class HardwareInterface : MonoBehaviour
     {
         if (port == null) return;
         if (!port.IsOpen || !connected) return;
+        cubeTimeoutTimer += Time.deltaTime;
 
         if (inMessages.Count > 0)
         {
@@ -48,40 +57,95 @@ public class HardwareInterface : MonoBehaviour
                 Debug.Log("Too many messages from cube! Skipping some...");
                 inMessages.Clear();
             }
-            if (message[0] == 'q')
+            switch (message[0])
             {
-                message = message.TrimStart('q');
-                var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
-                culture.NumberFormat.NumberDecimalSeparator = ".";
-                string[] parts = message.Split('_');
-                if (debugQuaternion)
-                {
-                    Debug.Log("Incoming Quaternion:\nw" + parts[0] + " x" + parts[1] + " y" + parts[2] + " z" + parts[3]);
-                }
-                cubeRotation.w = -float.Parse(parts[0], culture);
-                cubeRotation.x = float.Parse(parts[1], culture);
-                cubeRotation.y = float.Parse(parts[3], culture);
-                cubeRotation.z = float.Parse(parts[2], culture);
-                cubeRotation.Normalize();
-
-            }
-            else
-            {
-                Debug.Log("Incoming message from Cube\n" + message);
+                case 'q':
+                    message = message.TrimStart('q');
+                    var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+                    culture.NumberFormat.NumberDecimalSeparator = ".";
+                    string[] parts = message.Split('_');
+                    if (debugQuaternion)
+                    {
+                        Debug.Log("Incoming Quaternion:\nw" + parts[0] + " x" + parts[1] + " y" + parts[2] + " z" + parts[3]);
+                    }
+                    cubeRotation.w = -float.Parse(parts[0], culture);
+                    cubeRotation.x = float.Parse(parts[1], culture);
+                    cubeRotation.y = float.Parse(parts[3], culture);
+                    cubeRotation.z = float.Parse(parts[2], culture);
+                    cubeRotation.Normalize();
+                    break;
+                case 'v':
+                    Debug.Log("Incoming voltage:\n" + message);
+                    message = message.TrimStart('v');
+                    voltage = int.Parse(message);
+                    cubeTimeoutTimer = 0f;
+                    break;
+                default:
+                    Debug.Log("Unhandled incoming message from Cube\n" + message);
+                    break;
             }
         }
     }
 
+    /* ################################
+     * ####### Public Methods #########
+     * ################################ */
+
     public void Connect()
     {
-        connectionHandlerThread = new Thread(T_OpenConnection);
-        connectionHandlerThread.Start();
+        if (!connected)
+        {
+            if (!connectionAttempt)
+            {
+                Debug.Log("Connecting to Cube...");
+                connectionAttempt = true;
+                connectionHandlerThread = new Thread(T_OpenConnection);
+                connectionHandlerThread.Start();
+            }
+            else
+            {
+                Debug.Log("Unable to connect to Cube!\nConnection attempt in progress.");
+            }
+        }
+        else
+        {
+            Debug.Log("Unable to connect to Cube!\nCube already connected.");
+        }
     }
 
+    [SecurityPermissionAttribute(SecurityAction.Demand, ControlThread = true)]
     public void Disconnect()
     {
-        abortConnect = true;
+        if (connected)
+        {
+            connected = false;
+            connectionAttempt = false;
+
+            if (connectionHandlerThread != null) { connectionHandlerThread.Abort(); }
+            if (communicationHandlerThread != null) { communicationHandlerThread.Abort(); }
+
+            if (port != null)
+            {
+                if (port.IsOpen)
+                {
+                    port.WriteLine("+DISC");
+                    port.Close();
+                }
+            }
+            Debug.Log("Cube disconnected");
+        }
+        else
+        {
+            Debug.Log("Unable to disconnect from Cube!\nCube not connected.");
+        }
+
+    }
+
+    [SecurityPermissionAttribute(SecurityAction.Demand, ControlThread = true)]
+    public void CancelConnectionAttempt()
+    {
         connected = false;
+        connectionAttempt = false;
 
         if (connectionHandlerThread != null) { connectionHandlerThread.Abort(); }
         if (communicationHandlerThread != null) { communicationHandlerThread.Abort(); }
@@ -91,21 +155,85 @@ public class HardwareInterface : MonoBehaviour
             if (port.IsOpen)
             {
                 port.WriteLine("+DISC");
-                port.Close();
             }
+            port.Close();
+            Debug.Log("Port closed");
         }
-        Debug.Log("Cube disconnected");
+        Debug.Log("Connection attempt cancelled");
     }
 
     public void SendCommand(string message)
     {
-        Debug.Log("Sending command\n" + message);
+        Debug.Log("Sending command:\n" + message);
         outMessages.Enqueue(message);
     }
 
+    public void SetAllLeds(CubeColor c)
+    {
+        SendCommand("ar" + c.r + "g" + c.g + "b" + c.b);
+    }
+    public void FadeAllLeds(CubeColor c, int t)
+    {
+        SendCommand("far" + c.r + "g" + c.g + "b" + c.b + "t" + t);
+    }
+
+    public void SetLed(int f, CubeColor c)
+    {
+        SendCommand("l" + f + "r" + c.r + "g" + c.g + "b" + c.b);
+
+    }
+    public void FadeLed(int f, CubeColor c, int t)
+    {
+        SendCommand("fo" + f + "r" + c.r + "g" + c.g + "b" + c.b + "t" + t);
+    }
+
+    public void SetLedBrightness(int b)
+    {
+        brightness = b;
+        SendCommand("b" + b);
+    }
+
+    public void IncreaseLedsBrightness()
+    {
+        brightness = brightness + 20;
+        if (brightness > 200) brightness = 200;
+        SetLedBrightness(brightness);
+    }
+
+    public void DecreaseLedsBrightness()
+    {
+        brightness = brightness - 20;
+        if (brightness < 20) brightness = 20;
+        SetLedBrightness(brightness);
+    }
+
+    public bool IsConnected()
+    {
+        return connected;
+    }
+
+    public Quaternion GetRotation()
+    {
+        return cubeRotation;
+    }
+
+    public int GetVoltage()
+    {
+        return voltage;
+    }
+
+    public int GetLedBrightness()
+    {
+        return brightness;
+    }
+
+    /* ################################
+     * ####### Private Methods ########
+     * ################################ */
+
     private void T_OpenConnection()
     {
-        while (!abortConnect)
+        while (connectionAttempt)
         {
             string[] ports;
             if (fixedCOMPort)
@@ -126,7 +254,7 @@ public class HardwareInterface : MonoBehaviour
             //cycle through all the com ports
             for (int i = 0; i < ports.Length; i++)
             {
-                if (abortConnect) return;
+                if (!connectionAttempt) return;
                 port = new SerialPort(ports[i], baudRate);
                 port.ReadTimeout = 100;
                 port.WriteTimeout = 100;
@@ -138,23 +266,29 @@ public class HardwareInterface : MonoBehaviour
                         if (TestForCube())
                         {
                             print(ports[i] + "\nconnected successfully");
-                            port.WriteLine("ar0g0b0");
+                            SetAllLeds(CubeColor.black);
+                            SetLedBrightness(defaultBrightness);
                             connected = true;
+                            connectionAttempt = false;
                             communicationHandlerThread = new Thread(T_SendReceive);
                             communicationHandlerThread.Start();
                             return;
                         }
                         port.Close();
                     }
+                    port.Close();
                 }
-                catch { Debug.Log(ports[i] + ": failed"); }
+                catch
+                {
+                    port.Close();
+                    Debug.Log(ports[i] + ": failed"); }
             }
         }
     }
 
     private void T_SendReceive()
     {
-        while (connected && !abortConnect && port.IsOpen)
+        while (connected && port.IsOpen)
         {
             if (port.BytesToRead > 0)
             {
@@ -165,7 +299,13 @@ public class HardwareInterface : MonoBehaviour
             {
                 port.WriteLine(outMessages.Dequeue());
             }
-
+            if(cubeTimeoutTimer > connectionTimeout)
+            {
+                port.Close();
+                Debug.Log("Cube connection timed out!");
+                cubeTimeoutTimer = 0f;
+                Disconnect();
+            }
             Thread.Sleep(10);
         }
     }
@@ -185,20 +325,5 @@ public class HardwareInterface : MonoBehaviour
     private void OnApplicationQuit()
     {
         Disconnect();
-    }
-
-    public bool IsConnected()
-    {
-        return connected;
-    }
-
-    public Quaternion GetRotation()
-    {
-        return cubeRotation;
-    }
-
-    public int GetVoltage()
-    {
-        return voltage;
     }
 }
--- GyroGame/Assets/Scripts/InterfaceSampleScript.cs
+++ GyroGame/Assets/Scripts/InterfaceSampleScript.cs
@@ -4,73 +4,78 @@ using UnityEngine;
 
 public class InterfaceSampleScript : MonoBehaviour
 {
-
     public GameObject Cube1;
     public GameObject Cube2;
 
-    private Quaternion qNull = Quaternion.identity;
+    private Quaternion qOffset = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
     {
-        HardwareInterface.active.Connect();
+        //HardwareInterface.active.Connect();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cube1.transform.rotation = HardwareInterface.active.cubeRotation;
-        Cube2.transform.rotation = HardwareInterface.active.cubeRotation * qNull;
+        Cube1.transform.rotation = HardwareInterface.active.GetRotation();
+        Cube2.transform.rotation = HardwareInterface.active.GetRotation() * qOffset;
 
         HandleKeyInput();
     }
 
     private void HandleKeyInput()
     {
-        if (Input.GetKeyDown(KeyCode.N))
+        // Connect / Disconnect
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            qNull = Quaternion.Inverse(HardwareInterface.active.cubeRotation);
-
-            //port.WriteLine("c");
-
+            HardwareInterface.active.Connect();
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            HardwareInterface.active.SendCommand("far0g0b0nt1000");
-        }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            HardwareInterface.active.SendCommand("far255g100b0t1000");
-        }
-
         if (Input.GetKeyDown(KeyCode.D))
         {
-            HardwareInterface.active.Disconnect();
+            if (HardwareInterface.active.IsConnected())
+            {
+                HardwareInterface.active.Disconnect();
+            }
+            else
+            {
+                HardwareInterface.active.CancelConnectionAttempt();
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.G))
+        // Color changes
+
+        /* ### Lights fade off ### */
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //port.WriteLine("farg255b100t1000");
+            HardwareInterface.active.FadeAllLeds(CubeColor.black, 1000);
         }
-
+        /* ### All RED ### */
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            HardwareInterface.active.SetAllLeds(CubeColor.red);
+        }
+        /* ### All fade to BLUE ### */
         if (Input.GetKeyDown(KeyCode.B))
         {
-            //port.WriteLine("far100g0b255t1000");
+            HardwareInterface.active.FadeAllLeds(CubeColor.black, 1000);
+        }
+        /* ### All fade to ORANGE ### */
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            HardwareInterface.active.FadeAllLeds(CubeColor.orange, 1000);
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+
+        /* ### Example of the calculation of an offset for calibration, fully functional ### */
+        if (Input.GetKeyDown(KeyCode.N))
         {
-            //port.WriteLine("ar255g0b0");
+            qOffset = Quaternion.Inverse(HardwareInterface.active.GetRotation());
+
+            /* ### Send a singe 'c' to reinitialize gyro in cube firmware ### */
+            //port.WriteLine("c");
         }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //port.WriteLine("ar0g255b0");
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //port.WriteLine("ar0g0b255");
-        }
+
 
         if (Input.GetKeyDown(KeyCode.I))
         {
@@ -81,21 +86,13 @@ public class InterfaceSampleScript : MonoBehaviour
             //port.WriteLine("b220");
         }
 
-        if (Input.GetKeyDown(KeyCode.Y))
+        /* ### Fade to individual colors ### */
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            //port.WriteLine("fo0r0g255b100t1000");
-        }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            //port.WriteLine("fo1r0g10b100t1000");
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            //port.WriteLine("fo2r80g255b100t1000");
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            //port.WriteLine("fo3r140g255b0t1000");
+            HardwareInterface.active.FadeLed(0, new CubeColor(0, 255, 100), 1000);
+            HardwareInterface.active.FadeLed(1, new CubeColor(0, 10, 100), 1000);
+            HardwareInterface.active.FadeLed(2, new CubeColor(80, 255, 100), 1000);
+            HardwareInterface.active.FadeLed(3, new CubeColor(140, 255, 0), 1000);
         }
     }
 }
