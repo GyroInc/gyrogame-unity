338091b9ca25b280e68efcc3fe106ac3f39a94fb
KoenigSimon <koenig.simon@t-online.de>
2019-12-08 01:01:00 +0100

    First step to calibrate cube in game

---
+++ GyroGame/Assets/Scripts/OffsetTestScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetTestScript : MonoBehaviour
+{
+    public Transform absoluteCube;
+    public Transform relativeCube;
+
+    Quaternion correctedCube;
+    Quaternion playerPerspectiveCorrection = Quaternion.identity;
+
+    int calibrationStep = 0;
+    float yawOffset = 0;
+
+    private void Start()
+    {
+        HardwareInterface.active.Connect();
+    }
+
+    void Update()
+    {
+        if (HardwareInterface.active.IsConnected())
+        {
+            absoluteCube.rotation = HardwareInterface.active.GetRotation();
+            correctedCube = Quaternion.Euler(new Vector3(0, yawOffset, 0)) * HardwareInterface.active.GetRotation();
+
+            relativeCube.rotation = playerPerspectiveCorrection * correctedCube;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                switch(calibrationStep)
+                {
+                    case 0:
+                        HardwareInterface.active.SetLed(5, CubeColor.green);
+                        HardwareInterface.active.SetLed(1, CubeColor.orange);
+                        calibrationStep++;
+                        break;
+                    case 1:
+                        yawOffset = HardwareInterface.active.GetRotation().eulerAngles.y;
+                        HardwareInterface.active.SetLed(5, CubeColor.black);
+                        HardwareInterface.active.SetLed(1, CubeColor.black);
+                        break;
+                }                
+            }
+        }
+    }
+
+    public void CalculatePlayerPerspectiveCorrection()
+    {
+        //tbd
+    }
+}
