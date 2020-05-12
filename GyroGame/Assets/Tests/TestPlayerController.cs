70ff4db11689eb6eebf410f28e9a0b4da0ba7313
KoenigSimon <koenig.simon@t-online.de>
2020-05-12 18:17:30 +0200

    Added a test

---
+++ GyroGame/Assets/Tests/TestPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestPlayerController
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestPlayerControllerSimplePasses()
+        {
+            //Cannot interact with unity Classes
+            //aka cannot test anything
+            Assert.IsTrue(true);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestPlayerControllerWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            Assert.IsTrue(true);
+            yield return null;
+        }
+    }
+}
