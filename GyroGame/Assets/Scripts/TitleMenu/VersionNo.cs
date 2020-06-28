5bdeccbb32c6ab4e9bed4ccb45f5842defa5b17c
Manuel Piroch <manuel@pepit.net>
2020-06-28 13:18:01 +0200

    added version info to title menu

---
+++ GyroGame/Assets/Scripts/TitleMenu/VersionNo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class VersionNo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = Application.version;   
+    }
+}
